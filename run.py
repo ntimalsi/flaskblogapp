ccc4af320aec8c678116c7c68f32b37eb6cc39a5
Nischal <nischal.timalsina@gmail.com>
2022-03-26 13:16:33 -0400

    Custom Error Pages

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 #.\env\Scripts\activate.ps1
 
-from flaskblog import app
+from flaskblog import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
